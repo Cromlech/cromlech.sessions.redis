d4029093a4a80b93b32be1d902eb80e7cbd32740
Souheil CHELFOUH <trollfot@gmail.com>
2017-12-19 19:41:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+cromlech.sessions.redis
+***********************
+
+Implementation of HTTP sessions using Redis as a backend with a
+session id that is secured by a signature.
---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+install_requires = [
+    'setuptools',
+    'cromlech.marshallers',
+    'itsdangerous',
+    'biscuits',
+    'redis',
+]
+
+tests_require = [
+    'WebTest',
+]
+
+setup(
+    name='cromlech.sessions.redis',
+    version=version,
+    description="Session handling for wsgi applications using redis",
+    long_description=(
+        open("README.txt").read() + "\n" +
+        open(os.path.join("docs", "HISTORY.txt")).read()),
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='Cromlech Session',
+    author='The Dolmen team',
+    author_email='dolmen@list.dolmen-project.org',
+    url='http://gitweb.dolmen-project.org/',
+    license='ZPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['cromlech', 'cromlech.sessions'],
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=tests_require,
+    install_requires=install_requires,
+    extras_require={'test': tests_require},
+)
